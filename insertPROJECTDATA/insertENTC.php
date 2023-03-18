217033d7dbfa20e170d5809dec4d565d1a4f9dff
Vrushali Sirsath <95585241+Vrushalisirsat@users.noreply.github.com>
2023-03-18 14:33:47 +0530

    Initial commit

---
+++ insertPROJECTDATA/insertENTC.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Form page</title>
+    <link rel="stylesheet" type="text/css" href="one.css">
+</head>
+
+
+
+<body>
+<form action="insertENTC.php"  method="POST">
+    <div>   
+        
+        <div class="one">
+
+            <h1>Enter project Details</h1>
+
+        </div>
+
+        <div class="container">
+
+            <div class="two">
+                <h3><span>Students_Name</span><br></h3>
+                <textarea name="students_name" type="text" cols="70" rows="7"></textarea>
+            </div>
+
+            <div class="three">
+                <h3><label for="project_name">Project_Name</label></h3>
+                <input type="text" class="form-control" id="project_name" name="project_name" />
+            </div>
+
+            <div class="four">
+                <h3><label for="deparnment">Deparnment</label></h3>
+                <input type="text" class="form-control" id="deparnment" name="deparnment" />
+            </div>
+
+            <div class="five">
+                <h3><label for="guided_by">Guided_By</label></h3>
+                <input type="text" class="form-control" id="guided_by" name="guided_by" />
+
+
+                </table>
+            </div>
+
+            <div class="six">
+                <td colspan="2" rowspan="2">
+                    <label id="project_details">
+                        <h3><span>Project_Details</span><br></h3>
+                        <textarea name="project_details" type="text" cols="70" rows="8"></textarea>
+
+                </td>
+            </div>
+
+            <form>
+                <div class="seven">
+                    <h2><input type="Submit" value="SUBMIT" name="submit"></h2>
+                </div>
+            </form>
+
+            <div class=" panel footer text-right">
+                <small> @Ten Coder Squad</small>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+<br>
+
+
+
+
+ <!-- Signup form php -->
+ <?php
+
+ #create connection
+       $con =mysqli_connect('localhost','root','','php_college');
+ 
+ 
+ 
+       if(isset($_POST['submit']))
+       {
+          $studname=$_POST['students_name'];
+          $projectname=$_POST['project_name'];
+          $dept=$_POST['deparnment'];
+          $guidby=$_POST['guided_by'];  
+          $projectdetails=$_POST['project_details'];
+    
+        
+          $query="INSERT INTO mainentc(students_name,project_name,deparnment,guided_by,project_details) VALUES ('$studname','$projectname','$dept','$guidby','$projectdetails')";
+          $run=mysqli_query($con,$query);
+
+          //$sql="select username,password from login where username='$email' and password='$password'";
+         //  $result=mysqli_query($con,$sql);
+ 
+         
+          if(($run)>0)
+          {
+             echo "Data Inserted Succesfully";
+          }
+          else
+          {
+             echo "Invalid Data";
+          }
+ 
+       }
+       ?>
+ 
+ <br>
+ 
+ 
+ <a href="/beproject" style=" padding-left: 300px; text-align:bottom; " >Back to home</a>
+ 
+ 
+
+
+
+
+
+ </form>
+
+
+</body>
+
+</html>
---
+++ php_college/login_page/login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container vh-100">
+        <div class="row justify-content-center h-100">
+            <div class="card w-25 my-auto shadow" >
+                <div class="card-header text-center bg-primary text-white">
+                    <h2>Login Form</h2>
+                </div>
+                <div class="card-body">
+                    <form action="login.php" method="POST">
+                        <div class="form-group">
+                            <label for="email">Email Id</label>
+                            <input type="email" id="email" class="form-control" name="email" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" class="form-control" name="password" required />
+                        </div>
+                        <input type="submit" class="btn btn-primary w-100" value="logIn" name="submit">    
+                        <div class="signup-link">
+                         New User ? <a href="signup.php">Signup now</a>
+                        </div>
+
+                        
+                        <!-- LOGIN FORM -->
+<?php
+   
+   #create connection
+   $con =mysqli_connect('localhost','root','','php_college');
+   session_start();
+   if(isset($_POST['submit']))
+         {
+            $email=$_POST['email'];
+            $password=$_POST['password'];
+            // $query="INSERT INTO login(username,password) VALUES ('$email','$password')";
+            // $run=mysqli_query($con,$query);
+   
+            $sql="select email,password from login where email='$email' and password='$password'";
+            $result=mysqli_query($con,$sql);
+            
+            if(mysqli_num_rows($result)>0)
+            {
+               $_SESSION['email']=$email; 
+               echo "Login Succesfully";
+               echo "<script>window.location.href='/beproject'</script>";
+               die; 
+            } 
+            else
+            {
+               echo "Incorrect Username and Password";
+            }
+        }
+   
+      
+   ?>
+
+   
+                    </form>
+
+                </div>
+                
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+  <title>Admin login</title>
+
+  <link rel="stylesheet" href="login.css">
+  
+</head>
+<body>
+         <div class="loginbox">
+         <img src="WhatsApp Image 2022-01-09 at 7.49.05 PM (1).jpeg" class="avatar">
+         <h1>Login Here</h1>
+         <form>
+               <p> Official ID</p>
+               <input type="text" name="" placeholder="Enter Official ID">
+               <p>Password</p>
+               <input type="password" name="" placeholder="Enter Password">
+               <input type="submit" name="" value="Login">
+               <a href="#">Forgetten Password</a><br><br>
+               <a href="#">Don't have an account?</a>
+         </form>
+         
+         </div>
+</body>
+</html> -->
